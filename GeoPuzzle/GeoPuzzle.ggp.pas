1a186e549d83b169208d9610fbc8c1836c7eb6fe
Radoslav Bielik <immutability@bielik.org>
2013-03-04 21:38:17 +0100

    Version bumped to 2.0.8

--- GeoPuzzle/GeoPuzzle.ggp.pas
+++ GeoPuzzle/GeoPuzzle.ggp.pas
@@ -4,7 +4,7 @@ http://tiny.cc/osnica
 
 GeoPuzzle, http://geotrophy.net
 
-Verzia: 2.0.7 / 2013-03-04
+Verzia: 2.0.8 / 2013-03-04
 }
 
 uses simplexml;
@@ -12,7 +12,7 @@ uses simplexml;
 {$I GeoPuzzle.config.pas}
 
 // verziu needitovat rucne! je automaticky aktualizovana cez ant
-const VERSION = '2.0.7';
+const VERSION = '2.0.8';
 
 // zakladna URL pre XML subory jednotlivych puzzle
 const BASE_URL = 'http://geotrophy.net/xml/';
